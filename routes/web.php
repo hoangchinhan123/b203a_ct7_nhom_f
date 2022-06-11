0e530494c6e77c959169953f87ab6b555535db6b
nhan <chinhanhoan@gmail.com>
2022-06-11 08:20:32 +0700

    link

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get("/","KiemTra@getMaster")->name("gioithieu");
 //Trang chủ
 Route::get("GioiThieu","KiemTra@getMaster")->name("gioithieu");
 Route::post('tim-kiem','KiemTra@search')->name("timkiem");
